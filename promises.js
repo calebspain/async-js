b772dac85a96bcbb7e9253798fe4cb635456724b
Caleb Spain <crspain@uh.edu>
2019-04-28 19:46:20 -0500

    async js refresher

---
+++ promises.js
@@ -0,0 +1,60 @@
+const posts = [
+    { title: 'Post one', body: 'This is post one' },
+    { title: 'Post two', body: 'This is post two' }
+];
+
+let getPosts = () => {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+let createPost = (post) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+
+            const error = false;
+
+            if (!error) {
+                resolve();
+            } else {
+                reject('Error: Something went wrong');
+            }
+
+        }, 2000);
+    });
+}
+
+
+// createPost({ title: 'Post three', body: 'This is post three' })
+//     .then(getPosts)
+//     .catch(err => console.log(err));
+
+// let init = async () => {
+//     await createPost({ title: 'Post three', body: 'This is post three' });
+//     getPosts();
+// }
+
+// init();
+
+// let fetchUsers = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await res.json();
+//     console.log(data);
+// }
+
+// fetchUsers();
+
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000, 'Goodbye');
+// });
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) => console.log(values));
